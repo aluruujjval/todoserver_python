fee03d36d63faa7ffd694cb7e914906c7f40c961
ujjval <ujjval@abcd.com>
2018-09-14 00:14:53 +0530

    mysql description

--- todoserver_python/settings.py
+++ todoserver_python/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
    # 'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
     #    'ENGINE': 'django.db.backends.postgresql',
+    #    'ENGINE': 'django.db.backends.mysql',
      #   'NAME':' spring_hibernate',
       #  'USER': 'postgres_ujjval',
        
